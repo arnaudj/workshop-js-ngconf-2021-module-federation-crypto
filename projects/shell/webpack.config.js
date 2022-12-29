a44310973505c0e591426972237f968d482d49a9
Arnaud J <github@arnaud.dev>
2022-12-29 18:16:00 +0100

    feat: dynamic module federation: basic usage
    https://github.com/angular-architects/module-federation-plugin/blob/main/libs/mf/tutorial/tutorial.md#part-4-switch-to-dynamic-federation

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     new ModuleFederationPlugin({
       // For hosts (please adjust)
       remotes: {
-        mfe1: "mfe1@http://localhost:3000/remoteEntry.js",
+        //mfe1: "mfe1@http://localhost:3000/remoteEntry.js",
       },
 
       shared: share({
